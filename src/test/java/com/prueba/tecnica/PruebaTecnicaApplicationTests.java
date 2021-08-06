86cbd54df491e4276a536502d5277eeb3a6653c4
rmanjarres <rmanjarres@heinsohn.com.co>
2021-08-05 21:30:20 -0500

    Se agrego la carpeta src y la parte test

---
+++ src/test/java/com/prueba/tecnica/PruebaTecnicaApplicationTests.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+//Aqui van las pruebas
+@SpringBootTest
+class PruebaTecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
